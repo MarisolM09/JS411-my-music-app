99246d1682741adfe2b3252a97741b6c07a01a23
MarisolM09 <mmendez9980@gmail.com>
2022-08-16 20:27:03 -0500

    separated cards and drop menu and slider values

--- src/components/SoundCard.js
+++ src/components/SoundCard.js
@@ -4,10 +4,8 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 
-
 export default function SoundCard(props) {
   const { title, body } = props;
-  
 
   return (
     <Card sx={{ maxWidth: 275 }}>
@@ -18,10 +16,16 @@ export default function SoundCard(props) {
         <div className="card-title">
           <Typography variant="body2">{body}</Typography>
         </div>
+        <div className="Drop-menu">
+          <select>
+            <option value="Low">Low</option>
+            <option value="Normal">Normal</option>
+            <option value="High">High</option>
+          </select>
+        </div>
       </CardContent>
-      <CardActions>
-        {/* <Switch defaultChecked onChange={() => props.setState(!props.state)} /> */}
-      </CardActions>
+
+      <CardActions></CardActions>
     </Card>
   );
 }
